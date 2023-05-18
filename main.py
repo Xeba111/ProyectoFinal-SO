911102a97ee8bae6c82deb26516aecec37bcc9d2
SHINIK90 <73261988+SHINIK90@users.noreply.github.com>
2023-05-18 03:10:59 -0500

    Aesthetic ;)

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import threading
 import time
 import psutil
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTabWidget, QWidget, QVBoxLayout, QLabel, QHeaderView, QScrollArea, \
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTabWidget, QWidget, QVBoxLayout, QGridLayout, QLabel, QHeaderView, QScrollArea, \
     QTableWidget, QTableWidgetItem
 from PyQt5.QtCore import QTimer, QCoreApplication
 import pyqtgraph as pg
@@ -17,16 +17,17 @@ class App(QMainWindow):
 
     def initUI(self):
         self.setWindowTitle(self.title)
-        self.setGeometry(10, 10, 700, 500)
-        self.setMinimumSize(700,500)
-        self.setStyleSheet("background-color: #141414; color: #dddddd")
+        self.setGeometry(10, 60, 900, 500)
+        self.setMinimumSize(900,500)
+        self.setStyleSheet("background-color: #111111; color: #dddddd")
         self.table_widget = MyTableWidget(self)
         self.setCentralWidget(self.table_widget)
         self.table_widget.setStyleSheet(
-            "QTabWidget::pane { background-color: #ECECEC; }"
-            "QTabBar::tab { background-color: #240032; border: 1px solid black; padding:5px 100% 5px }"
-            "QTabBar::tab:selected { background-color: #440065; }"
+            "QTabWidget::pane { background-color: white; }"
+            "QTabBar::tab { background-color: #240032; border-right: 1px solid grey; padding:5px 100% 5px; }"
+            "QTabBar::tab:selected { background-color: #330044; }"
         )
+              
         self.show()
 
 
@@ -39,12 +40,20 @@ class MyTableWidget(QWidget):
         self.tabs = QTabWidget()
         self.tab1 = QWidget()
         self.tab2 = QWidget()
-        self.tabs.resize(300, 200)
+        self.tab1.setStyleSheet(
+            "background-color:#160020; border: 1px solid #333333"
+        )
+        self.tab2.setStyleSheet(
+            "background-color:#160020; border: 1px solid #333333"
+        )
 
 
         # Add tabs
         self.tabs.addTab(self.tab1, "Processes")
         self.tabs.addTab(self.tab2, "Performance")
+        self.tabs.setStyleSheet(
+            "QTabWidget::pane:selected { background-color: red; }"  # Selected tab content background color
+        ) 
 
         # Create first tab
         self.tab1.layout = QVBoxLayout(self)
@@ -61,16 +70,60 @@ class MyTableWidget(QWidget):
         self.tab1.setLayout(self.tab1.layout)
 
         # Create second tab
-        self.tab2.layout = QVBoxLayout(self)
-        self.cpu_plot = pg.PlotWidget(title="CPU Usage (%)")
-        self.ram_plot = pg.PlotWidget(title="RAM Usage (%)")
-        self.disk_plot = pg.PlotWidget(title="Disk Usage (%)")
-        self.network_plot = pg.PlotWidget(title="Network Bandwidth Usage (MB)")
+        self.tab2.layout = QGridLayout(self)
+        self.cpu_plot = pg.PlotWidget(title="CPU Usage (%) per Minute")
+        self.ram_plot = pg.PlotWidget(title="RAM Usage (%) per Minute")
+        self.disk_plot = pg.PlotWidget(title="Disk Usage (%) per Minute")
+        self.network_plot = pg.PlotWidget(title="Network Bandwidth Usage (MB) per Minute")
 
-        self.tab2.layout.addWidget(self.cpu_plot)
-        self.tab2.layout.addWidget(self.ram_plot)
-        self.tab2.layout.addWidget(self.disk_plot)
-        self.tab2.layout.addWidget(self.network_plot)
+        self.cpu_plot.setMaximumHeight(300)
+        self.ram_plot.setMaximumHeight(300)
+        self.disk_plot.setMaximumHeight(300)
+        self.network_plot.setMaximumHeight(300)
+
+        self.cpu_plot.setYRange(0, 100)
+        self.ram_plot.setYRange(0, 100)
+        self.disk_plot.setYRange(0, 100)
+        self.network_plot.setYRange(0, 100)
+
+        self.cpu_plot.setXRange(-60, 0)
+        self.ram_plot.setXRange(-60, 0)
+        self.disk_plot.setXRange(-60, 0)
+        self.network_plot.setXRange(-60, 0)
+
+        self.cpu_plot.setMouseEnabled(y=False, x=True)
+        self.ram_plot.setMouseEnabled(y=False, x=True)
+        self.disk_plot.setMouseEnabled(y=False, x=True)
+        self.network_plot.setMouseEnabled(y=False, x=True)
+
+        self.cpu_plot.hideButtons()
+        self.ram_plot.hideButtons()
+        self.disk_plot.hideButtons()
+        # self.network_plot.hideButtons()
+
+        def restrictPanning(self, plot_item):
+            def onRangeChanged():
+                x_range = plot_item.viewRange()[0]
+                if x_range[0] < -3600:
+                    # Adjust the range if it exceeds the allowed limits
+                    plot_item.setXRange(max(x_range[0], -60), min(x_range[1], 0))
+                if x_range[1] > 0:
+                    # Adjust the range if it exceeds the allowed limits
+                    plot_item.setXRange(max(x_range[0], -60), min(x_range[1], 0))
+
+            plot_item.sigRangeChanged.connect(onRangeChanged)
+
+        # Call the restrictPanning method for each plot widget
+        restrictPanning(self,self.cpu_plot.plotItem)
+        restrictPanning(self,self.ram_plot.plotItem)
+        restrictPanning(self,self.disk_plot.plotItem)
+        restrictPanning(self,self.network_plot.plotItem)
+
+        self.tab2.layout.addWidget(self.cpu_plot,0,0)
+        self.tab2.layout.addWidget(self.ram_plot,0,1)
+        self.tab2.layout.addWidget(self.disk_plot,1,0)
+        self.tab2.layout.addWidget(self.network_plot,1,1)
+        self.tab2.layout.setSpacing(10)
         self.tab2.setLayout(self.tab2.layout)
 
         # Add tabs to widget
